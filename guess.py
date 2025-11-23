bcd015eb1d171cf80c27e6761b2d49a097326fc0
J-Obog <jobogbaimhe@gmail.com>
2025-11-22 19:30:24 -0500

    testing feedback

--- guess.py
+++ guess.py
@@ -5,7 +5,7 @@ candidates = []
 with open("known.json", "r", encoding="utf") as ifile:
     candidates = json.load(ifile)
 
-target_word = "honor"
+target_word = "brain"
 start_word = "rinse"
 
 not_in_target = set()
@@ -14,6 +14,33 @@ unknown_idxs = set()
 
 guesses = 0
 won = False
+
+def get_feedback(guess_word):
+    if len(guess_word) != 5:
+        raise Exception("word must be 5 characters")
+
+    mi = {}
+    ni = set()
+    mc = set()
+
+    for i in range(5):
+        if guess_word[i] == target_word[i]:
+            matched_char = guess_word[i]
+            if matched_char not in mi:
+                mi[matched_char] = []
+
+            mi[matched_char].append(i)
+    
+    guess_word_set = set(guess_word)
+    target_word_set = set(target_word)
+
+    ni = guess_word_set.difference(target_word_set)
+
+    #for simplicity, let's assume the target word doesn't have any repeating letters
+    mc = guess_word_set.intersection(target_word_set).difference(set(mi.keys()))
+
+    return {"mi": mi, "ni": ni, "mc": mc}
+
 
 while guesses < 6:
     guess_word = None
@@ -26,6 +53,9 @@ while guesses < 6:
         won = True
         break
 
+    print(get_feedback(guess_word))
+    
+    break
     guesses += 1 
 
 
